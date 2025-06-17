17e7980f312ab913214dc46413ed71f8a2d7e536
noah-silverberg <noah.silverberg@yale.edu>
2025-06-17 16:09:52 -0400

    Updated training launcher

--- pipeline/launcher.py
+++ pipeline/launcher.py
@@ -1,9 +1,4 @@
-# Implements Notebook 3 & 6: run helper for training apps
 import logging
-from .utils import ensure_dir
-from .config import CUDA_DEVICE
-from .config import MODEL_DIR
-import torch
 
 
 def run_app(app: str, args: list):
@@ -16,10 +11,10 @@ def run_app(app: str, args: list):
     argv.append(f"--batch_size=8")
     argv.extend(args)
     log = logging.getLogger("pipeline")
-    log.info(f"Running: {app} {argv}")
+    log.info(f"RUNNING: {app} {argv}\n")
     module_name, class_name = app.rsplit(".", 1)
     module = importlib.import_module(module_name)
     cls = getattr(module, class_name)
     instance = cls(argv)
     instance.main()
-    log.info(f"Finished: {app}")
+    log.info(f"\nFINISHED: {app}")

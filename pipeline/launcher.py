4306a36ee3275ecfa60c145c944f2bf6b11972dc
noah-silverberg <noah.silverberg@yale.edu>
2025-06-18 10:54:23 -0400

    Updated training launcher

--- pipeline/launcher.py
+++ pipeline/launcher.py
@@ -6,14 +6,11 @@ def run_app(app: str, args: list):
     """Run a training or other application via dynamic import and its main()."""
     # Prepend default args
     argv = []
-    argv.append(f"--num_workers=0")
-    argv.append(f"--batch_size=8")
     argv.extend(args)
-    log = logging.getLogger("pipeline")
-    log.info(f"RUNNING: {app} {argv}\n")
+    logging.info(f"RUNNING: {app} {argv}\n")
     module_name, class_name = app.rsplit(".", 1)
     module = importlib.import_module("pipeline." + module_name)
     cls = getattr(module, class_name)
     instance = cls(argv)
     instance.main()
-    log.info(f"\nFINISHED: {app}")
+    logging.info(f"\nFINISHED: {app}")

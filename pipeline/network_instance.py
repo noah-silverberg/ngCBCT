Fixed beta calculation and reading priors from config

pipeline/network_instance.py
@@ -3,6 +3,7 @@ import torch.nn as nn
 import torch.nn.functional as F
 from torch.nn import init
 import math
+import ast
 
 
 def init_weights(net, init_type='normal', gain=0.02):
@@ -1216,6 +1217,11 @@ class IResNetBBB(nn.Module):
     def __init__(self, img_ch=1, output_ch=1, prior_mu=0, prior_sigma=1e-2):
         super(IResNetBBB, self).__init__()
 
+        if isinstance(prior_mu, str):
+            prior_mu = ast.literal_eval(prior_mu)
+        if isinstance(prior_sigma, str):
+            prior_sigma = ast.literal_eval(prior_sigma)
+
         up_conv = True
 
         self.maxpool = nn.MaxPool2d(kernel_size=2, stride=2)
@@ -1313,5 +1319,3 @@ class IResNetBBB(nn.Module):
         if priors:
             return "Bayesian Priors:\n  " + "\n  ".join(priors)
         return ""
-    
-print(IResNetBBB())

pipeline/train_app_MK6_numpy.py
@@ -44,8 +44,11 @@ def init_model(config: dict):
     return model
 
 
-def init_loss(config: dict):
-    loss = nn.SmoothL1Loss()
+def init_loss(config: dict, is_bayesian: bool):
+    if is_bayesian:
+        loss = nn.SmoothL1Loss(reduction='sum')
+    else:
+        loss = nn.SmoothL1Loss(reduction='mean')
     return loss
 
 
@@ -214,7 +217,7 @@ class TrainingApp:
 
         # Initialize model, loss, and optimizer
         self.model = init_model(self.config)
-        self.criterion = init_loss(self.config)
+        self.criterion = init_loss(self.config, self.is_bayesian)
         self.optimizer, self.scheduler = init_optimizer(self.config, self.model)
 
         # Resume from checkpoint if provided
@@ -301,9 +304,9 @@ class TrainingApp:
 
                 if self.is_bayesian:
                     reconstruction_loss = self.criterion(train_outputs, train_truths)
-                    kl_loss = self.model.kl_divergence() / len(train_dl.dataset)
+                    kl_loss = self.model.kl_divergence() / len(train_dl)
                     train_loss = reconstruction_loss + self.config['beta_BBB'] * kl_loss
-                    if batch_idx % 100 == 0:
+                    if batch_idx % 500 == 0:
                         logger.debug(f"Epoch {epoch_ndx}, Batch {batch_idx}: Total Loss: {train_loss.item()}, Reconstruction Loss: {reconstruction_loss.item()}, KL Loss: {kl_loss.item()}, Beta: {self.config['beta_BBB']}")
                 else:
                     train_loss = self.criterion(train_outputs, train_truths)
@@ -372,7 +375,7 @@ class TrainingApp:
 
                     if self.is_bayesian:
                         reconstruction_loss = self.criterion(val_outputs, val_truths)
-                        kl_loss = self.model.kl_divergence() / len(val_dl.dataset)
+                        kl_loss = self.model.kl_divergence() / len(val_dl)
                         val_loss = reconstruction_loss + self.config['beta_BBB'] * kl_loss
                     else:
                         val_loss = self.criterion(val_outputs, val_truths)